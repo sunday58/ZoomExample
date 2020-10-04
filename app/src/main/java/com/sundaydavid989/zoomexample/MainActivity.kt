0d83bbc76a3fb63eb97278edda7dac523cfc2535
sundaydavid <sundaydavid989@gmail.com>
2020-10-04 12:28:11 +0100

    Initial commit

---
+++ app/src/main/java/com/sundaydavid989/zoomexample/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.sundaydavid989.zoomexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
